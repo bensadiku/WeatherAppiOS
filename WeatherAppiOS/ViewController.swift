89e5b06324b20f0773c617568793583b61016e2e
Ben sadiku <bensadiku65@gmail.com>
2019-09-14 06:38:54 -0700

    Final commit, finished app

--- WeatherAppiOS/ViewController.swift
+++ WeatherAppiOS/ViewController.swift
@@ -4,15 +4,16 @@
 //
 //  Created by ben on 13/09/2019.
 //  Copyright © 2019 ben. All rights reserved.
-//     
+//
 
+import Foundation
 import UIKit
 import Alamofire
 import SwiftyJSON
 import NVActivityIndicatorView
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dayLabel: UILabel!
@@ -22,28 +23,96 @@ class ViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     
     let gradientLayer = CAGradientLayer()
+    
+    //You can get a free API key from openweathermap.org
+    
+    let apikey = ""
+    
+    //default lat lon value, will be updated with the locationManager
+    var lat = 11.344533
+    var lon = 104.3332
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize )
+        
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        activityIndicator.startAnimating()
+        if(CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate  = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         setBlueGradientBackground()
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apikey)&units=metric").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                print("beni resposnse \(JSON(responseStr))")
+                //reponse objects
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                
+                //filling the views from the JSON response
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.conditionImageView.image = UIImage(named: iconName)
+                self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.temperatureLabel.text = "\(Int (round(jsonTemp["temp"].doubleValue)))"
+                
+                //setting the date
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEEE"
+                self.dayLabel.text  = dateFormatter.string(from: date)
+                
+                //background gradient, takes the suffix d/n from the response to update the gradient
+                let suffix = iconName.suffix(1)
+                if(suffix == "n"){
+                    self.setGrayGradientBackground()
+                }else {
+                    self.setBlueGradientBackground()
+                }
+            }
+        }
+        //stop updating location when we're done
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
     func setBlueGradientBackground() {
-        let topColor = UIColor(red: 95.0/255/0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
-        let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 104.0/255.0, alpha: 1.0).cgColor
+        let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [topColor, bottomColor]
     }
     
     func setGrayGradientBackground(){
-        let topColor = UIColor(red: 151.0/255/0, green: 151.0/255.0, blue: 151.0/255, alpha: 1.0).cgColor
+        let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
         let bottomColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [topColor, bottomColor]
     }
-
-
 }
